a041e256d03d6d4ce86143390aca710b5f3711c9
Erick Cisneros <erickxz10@gmail.com>
2019-08-03 12:23:12 -0500

    Primer deploy del chat

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const path = require('path');
+
+const express = require('express');
+const socketio = require('socket.io');
+
+const mongoose = require('mongoose');
+
+const app = express(); //Este es el servidor
+const server = http.createServer(app);
+const io = socketio.listen(server);
+
+mongoose.connect('mongodb://localhost/chat-databse')
+.then(db => console.log('db is connected'))
+.catch(err => console.log(err));
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+
+require('./sockets')(io);
+
+//Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Start the server
+server.listen(app.get('port'), () => {
+    console.log('server on port', app.get('port'));
+});
